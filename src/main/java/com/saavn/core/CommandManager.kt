f72461f53496fc0833a4c3c566ecd61a29ff64c9
DEVANSH GUPTA <132440071+XMGMAX@users.noreply.github.com>
2026-04-11 16:12:25 +0530

    Optimize command lookup and redesign README

--- src/main/java/com/saavn/core/CommandManager.kt
+++ src/main/java/com/saavn/core/CommandManager.kt
@@ -1,50 +1,46 @@
 package com.saavn.core
 
-import com.saavn.core.SaavnDna
-import kotlin.collections.ArrayList
-import kotlin.collections.MutableList
 import com.saavn.commandmeta.CommandBase
 import com.saavn.commandmeta.HelpCommand
+import com.saavn.commands.info.InviteCommand
+import com.saavn.commands.info.PingCommand
+import com.saavn.commands.music.JoinCommand
+import com.saavn.commands.music.PauseCommand
+import com.saavn.commands.music.PlayCommand
+import com.saavn.commands.music.QueueCommand
+import com.saavn.commands.music.RepeatCommand
+import com.saavn.commands.music.ResumeCommand
+import com.saavn.commands.music.SkipCommand
+import com.saavn.commands.music.StopCommand
+import com.saavn.commands.music.VolumeCommand
 import net.dv8tion.jda.api.JDA
-import org.jetbrains.annotations.Nullable
-
-// Importing Commands
-import com.saavn.commands.info.*
-import com.saavn.commands.music.*
 
 class CommandManager(bot: SaavnDna, jda: JDA) {
-    var commands: MutableList<CommandBase> = ArrayList<CommandBase>();
+    private val commandList: MutableList<CommandBase> = mutableListOf()
+    private val commandLookup: MutableMap<String, CommandBase> = mutableMapOf()
 
     init {
-        this.addCommand(PingCommand(jda))
-        this.addCommand(InviteCommand(jda))
-        this.addCommand(JoinCommand(jda))
-        this.addCommand(PlayCommand(jda))
-        this.addCommand(StopCommand(jda))
-        this.addCommand(SkipCommand(jda))
-        this.addCommand(RepeatCommand(jda))
-        this.addCommand(PauseCommand(jda))
-        this.addCommand(ResumeCommand(jda))
-        this.addCommand(VolumeCommand(jda))
-        this.addCommand(QueueCommand(jda))
+        registerCommand(PingCommand(jda))
+        registerCommand(InviteCommand(jda))
+        registerCommand(JoinCommand(jda))
+        registerCommand(PlayCommand(jda))
+        registerCommand(StopCommand(jda))
+        registerCommand(SkipCommand(jda))
+        registerCommand(RepeatCommand(jda))
+        registerCommand(PauseCommand(jda))
+        registerCommand(ResumeCommand(jda))
+        registerCommand(VolumeCommand(jda))
+        registerCommand(QueueCommand(jda))
 
-        this.addCommand(HelpCommand(jda, commands))
+        registerCommand(HelpCommand(jda, commandList))
     }
 
-    private fun addCommand(cmd: CommandBase) {
-        this.commands.add(cmd);
+    private fun registerCommand(command: CommandBase) {
+        commandList.add(command)
+        commandLookup[command.getName().lowercase()] = command
     }
 
-    @Nullable
     fun getCommand(search: String): CommandBase? {
-        val searchLower = search.lowercase()
-
-        for (cmd in this.commands) {
-            if (cmd.getName().equals(searchLower)) {
-                return cmd
-            }
-        }
-
-        return null
+        return commandLookup[search.lowercase()]
     }
 }
